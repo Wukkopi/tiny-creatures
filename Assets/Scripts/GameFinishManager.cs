6a63e564a9545cb9d2e6dad4fd53391ba919a504
Wukkopi <wukkopi@gmail.com>
2024-10-06 17:48:26 +0300

    Added win condition

--- Assets/Scripts/GameFinishManager.cs
+++ Assets/Scripts/GameFinishManager.cs
@@ -4,11 +4,37 @@ using UnityEngine;
 
 public class GameFinishManager : MonoBehaviour
 {
+    
+    [SerializeField]private BoxCollider2D finishArea;
+    private GameInitializer game;
+
+    private List<Collider2D> colliders = new List<Collider2D>();
+    private ContactFilter2D filter = new ContactFilter2D();
     // Start is called before the first frame update
     void Start()
     {
-        
+        game = GetComponent<GameInitializer>();
     }
+
+    void FixedUpdate()
+    {
+        finishArea.OverlapCollider(filter.NoFilter(), colliders);
+
+
+        var finishers = 0;
+        foreach(var collider in colliders)
+        {
+            if (collider.gameObject.layer == LayerMask.NameToLayer("Character"))
+                finishers++;
+        }
+        if (finishers > 0)
+           // Debug.Log($"FINISHERS: {finishers}");
+        if (finishers == game.AllCharacters.Count)
+        {
+            Debug.LogWarning("FINISH");
+        }    
+    }
+
 
     // Update is called once per frame
     void Update()
